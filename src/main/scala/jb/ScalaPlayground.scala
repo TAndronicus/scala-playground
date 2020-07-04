8dd1c93e952952c7350ca95264d2f4251e6146a2
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-07-04 19:33:56 +0200

    Initial commit

---
+++ src/main/scala/jb/ScalaPlayground.scala
@@ -0,0 +1,15 @@
+package jb
+
+// Created by https://github.com/justinhj/fp-starter-pack.g8
+// Simple ZIO App
+
+import zio.App
+import zio.console._
+
+object ScalaPlayground {
+
+  def main(args: Array[String]): Unit = {
+    println("Yo")
+  }
+
+}
